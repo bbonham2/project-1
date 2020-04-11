d43ef8f310d4bca38fecd1df3fa47c2269fb2e03
bbonham2 <bbonham2@uoregon.edu>
2020-04-10 20:55:17 -0500

    Add sumEvens to functions.js.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -23,3 +23,17 @@ let arrMax = arr => {
 
 console.log(arrMax([2, 3, 5, 7, 9]));
 console.log(arrMax([6, 2, 4]));
+//Req. 3d ========================
+let arr = [1, 2, 3, 4, 5];
+
+let sumEvens = arr => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sum = sum + arr[i];
+    }
+  }
+  return sum;
+};
+
+console.log(sumEvens(arr));
